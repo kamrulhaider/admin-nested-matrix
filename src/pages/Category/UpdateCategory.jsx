6dd403a3b9e1938f395880f17dbe526707208d55
Kamrul Haider Chowdhury <kamrulhaider.ssb@gmail.com>
2024-11-19 02:25:33 +0600

    Fix category update modal

--- src/pages/Category/UpdateCategory.jsx
+++ src/pages/Category/UpdateCategory.jsx
@@ -2,15 +2,10 @@
 import { useState } from "react";
 import { faMultiply } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-  Transition,
-  TransitionChild,
-} from "@headlessui/react";
+
 import { Fragment } from "react";
 import Input from "../../components/Input/Input";
+import { Dialog, Transition } from "@headlessui/react";
 
 export default function UpdateCategory({ Id, data, handleCategoryUpdate }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -43,7 +38,7 @@ export default function UpdateCategory({ Id, data, handleCategoryUpdate }) {
 
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-[1000]" onClose={() => {}}>
-          <TransitionChild
+          <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
             enterFrom="opacity-0"
@@ -53,11 +48,11 @@ export default function UpdateCategory({ Id, data, handleCategoryUpdate }) {
             leaveTo="opacity-0"
           >
             <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </TransitionChild>
+          </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <TransitionChild
+              <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
                 enterFrom="opacity-0 scale-95"
@@ -66,8 +61,8 @@ export default function UpdateCategory({ Id, data, handleCategoryUpdate }) {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <DialogPanel className="lg:w-[60%]  w-full mx-auto transform overflow-hidden rounded-md bg-white p-2 text-left align-middle shadow-xl transition-all">
-                  <DialogTitle
+                <Dialog.Panel className="lg:w-[60%]  w-full mx-auto transform overflow-hidden rounded-md bg-white p-2 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
                     as="h3"
                     className="flex items-center py-3 px-7 justify-between"
                   >
@@ -77,7 +72,7 @@ export default function UpdateCategory({ Id, data, handleCategoryUpdate }) {
                       onClick={closeModal}
                       className="cursor-pointer p-2"
                     />
-                  </DialogTitle>
+                  </Dialog.Title>
                   <hr />
                   <form onSubmit={handleSubmit}>
                     <div className="mt-5 px-7 mb-5">
@@ -105,8 +100,8 @@ export default function UpdateCategory({ Id, data, handleCategoryUpdate }) {
                       </div>
                     </div>
                   </form>
-                </DialogPanel>
-              </TransitionChild>
+                </Dialog.Panel>
+              </Transition.Child>
             </div>
           </div>
         </Dialog>
